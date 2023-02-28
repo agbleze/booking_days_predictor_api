7e915432c967b6901a1a868f733466a258b55c7c
agbleze <lagbleze@st.ug.edu.gh>
2023-02-28 23:14:45 +0100

    Create __init__.py

---
+++ booking_gauger/api/__init__.py
@@ -0,0 +1,13 @@
+# import os
+
+# print(os.getcwd())
+
+# def get_path(folder_name: str, file_name: str):
+#     cwd = os.getcwd()
+#     return f"{cwd}/{folder_name}/{file_name}"
+ 
+ 
+# model_path = get_path(folder_name='booking_gauger/api', file_name='model.model')
+# #model = joblib.load(filename=model_path)
+# print(model_path) 
+ 
