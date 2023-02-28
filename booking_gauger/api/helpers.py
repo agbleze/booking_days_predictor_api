e58b053148c224252de160c54779e85a14c394ce
agbleze <lagbleze@st.ug.edu.gh>
2023-02-28 20:54:41 +0100

    Update helpers.py

--- booking_gauger/api/helpers.py
+++ booking_gauger/api/helpers.py
@@ -1,12 +1,31 @@
 #%%
 import joblib
 import os
+import requests
+import json
+
+
+# function to get the full pathname for the data file
+
+def get_path(folder_name: str, file_name: str):
+    cwd = os.getcwd()
+    return f"{cwd}/{folder_name}/{file_name}"
+
 
 #%%
-model = joblib.load('new_model.model')
-#%%
+model_path = get_path(folder_name='booking_gauger/api', 
+                      file_name='model.model'
+                    )
+model = joblib.load(filename=model_path)
 
-def predict_booking(model, X):
+def predict_booking(num_sessions, city_encoded, country_encoded,
+                    device_class_encoded, instant_booking_encoded,
+                    user_verified_encoded, model=model
+                    ):
+    X = [num_sessions, city_encoded, country_encoded,
+         device_class_encoded, instant_booking_encoded,
+        user_verified_encoded
+        ]
     if type(X) is not list:
         raise Exception('X must be a list')
     if len(X) != 6:
@@ -18,10 +37,37 @@ def predict_booking(model, X):
     return prediction.tolist()
 
 
-# function to get the full pathname for the data file
+def request_prediction(URL: str, data: dict) -> int:
+    """
+    This function makes an API request to the booking gauger API and return
+    a prediction of number of days room will be booked for accommodation.
 
-def get_path(folder_name: str, file_name: str):
-    cwd = os.getcwd()
-    return f"{cwd}/{folder_name}/{file_name}"
+    Parameters
+    ----------
+    URL : Endpoint of booking gauger API
+        The API link.
+    data : dict
+        input data to be used for prediction. This should be a dictionary
+        with keys as num_sessions, city_encoded, country_encoded,
+         device_class_encoded, instant_booking_encoded and user_verified_encoded
+
+        num_sessions: is the number of sessions of an online client using platform 
+                      for booking accommodation
+        city_encoded: City from which client is accessing the online booking platform
+        country_encoded: Country from which client is accessing the online booking platform
+        device_class_encoded: Type of Device used by client to access the online booking platform
+        instant_booking_encoded: Whether client used instant booking button for the booking
+        user_verified_encoded: Whether the user is verified 
+                      
+    Returns
+    -------
+    int
+        prediction.
+
+    """
+    req = requests.post(url=URL, json=data)
+    response = req.content
+    prediction = json.loads(response)['predicted_value'][0]
+    return prediction
  
  
