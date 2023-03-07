c11c1df1236e2e3fda3aeee6393ed64065bf92da
agbleze <lagbleze@st.ug.edu.gh>
2023-03-07 20:34:15 +0100

    Update helpers.py

--- booking_gauger/api/helpers.py
+++ booking_gauger/api/helpers.py
@@ -3,9 +3,7 @@ import joblib
 import os
 import requests
 import json
-
-
-# function to get the full pathname for the data file
+import pandas as pd
 
 def get_path(folder_name: str, file_name: str):
     cwd = os.getcwd()
@@ -14,30 +12,25 @@ def get_path(folder_name: str, file_name: str):
 DEFAULT_URL = "http://192.168.0.168:8080/predict"
 
 #%%
-model_path = get_path(folder_name='booking_gauger/api', 
-                      file_name='model.model'
+model_path = get_path(folder_name='booking_gauger/api/model_store', 
+                      file_name='best_model.model'
                     )
 model = joblib.load(filename=model_path)
 
-def predict_booking(num_sessions, city_encoded, country_encoded,
-                    device_class_encoded, instant_booking_encoded,
-                    user_verified_encoded, model=model
+def predict_booking(device_class, city, country, 
+                    instant_booking, user_verified,
+                    num_sessions, model=model
                     ):
-    X = [num_sessions, city_encoded, country_encoded,
-         device_class_encoded, instant_booking_encoded,
-        user_verified_encoded
-        ]
-    if type(X) is not list:
-        raise Exception('X must be a list')
-    if len(X) != 6:
-        raise Exception('X must contain 6 values for \
-                        num_sessions, city, country, device, \
-                        instant booking, user verified'
-                        )
-    prediction = model.predict([X])
-    return prediction.tolist()
-
-
+        in_data = {'num_sessions': num_sessions, 'city': city, 'country': country,
+                    'device_class': device_class, 'instant_booking': instant_booking,
+                    'user_verified': user_verified
+                    }
+        
+        prediction_input_data = pd.DataFrame(data=in_data, index=[0])
+        prediction = model.predict(prediction_input_data)
+        return prediction.tolist()
+    
+    
 def request_prediction(data: dict, URL: str = DEFAULT_URL,) -> int:
     """
     This function makes an API request to the booking gauger API and return
